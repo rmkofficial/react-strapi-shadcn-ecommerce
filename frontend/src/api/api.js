271d8411deb997194b128f87d5b01edd01b7ea60
rmkofficial <kmert0@outlook.com>
2024-09-11 20:47:41 +0300

    Refactor backend package.json and contentTypes.d.ts

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-
-const API_URL = 'http://localhost:1337/api';
+// Heroku'daki Strapi API URL'si
+const API_URL = 'https://stylesage-1ee0b4549a17.herokuapp.com/api';
 
 // Ürünleri çekmek için API çağrısı
 export const fetchProducts = async () => {
@@ -12,7 +12,7 @@ export const fetchProducts = async () => {
             name: product.attributes.name,
             price: product.attributes.price,
             category: product.attributes.category,
-            imageUrl: `http://localhost:1337${product.attributes.image?.data[0]?.attributes?.url || ''}`,
+            imageUrl: `https://stylesage-1ee0b4549a17.herokuapp.com${product.attributes.image?.data[0]?.attributes?.url || ''}`,
         }));
     } catch (error) {
         console.error('Error fetching products:', error);
@@ -27,7 +27,7 @@ export const fetchBanners = async () => {
         return response.data.data.map((banner) => ({
             id: banner.id,
             title: banner.attributes.Title,
-            imageUrl: `http://localhost:1337${banner.attributes.Image?.data[0]?.attributes?.url || ''}`,
+            imageUrl: `https://stylesage-1ee0b4549a17.herokuapp.com${banner.attributes.Image?.data[0]?.attributes?.url || ''}`,
         }));
     } catch (error) {
         console.error('Error fetching banners:', error);
